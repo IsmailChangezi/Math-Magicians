377d79e7214fd57545c60e0c63c7fbcdf5c60085
Ismail Ali <ismailchangezi801@gmail.com>
2023-01-24 17:16:48 +0500

    Fix Linter Errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends Component {
   render() {
