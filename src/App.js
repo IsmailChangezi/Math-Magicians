8be417df14aac799cffd0cc176d5f049ed4ba925
Ismail Ali <ismailchangezi801@gmail.com>
2023-02-01 17:38:56 +0500

    Feature Full Website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Calculator from './components/calculator';
+import { Route, Routes } from 'react-router-dom';
+import RenderCalculator from './components/RenderCalculator';
+import Header from './components/Header';
+import Home from './components/Home';
+import Quotes from './components/Quotes';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Quotes" element={<Quotes />} />
+        <Route path="/Calculator" element={<RenderCalculator />} />
+      </Routes>
+    </>
   );
 }
 
